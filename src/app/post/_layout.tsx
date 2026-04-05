2f71b895e0997874694bb35f9390a5b111e73f1c
김지호 <rlawl3383@gmail.com>
2026-04-05 15:40:09 +0900

    authroute 추가

--- src/app/post/_layout.tsx
+++ src/app/post/_layout.tsx
@@ -1,3 +1,4 @@
+import AuthRoute from "@/components/AuthRoute";
 import { colors } from "@/constants";
 import { Feather } from "@expo/vector-icons";
 import { router, Stack } from "expo-router";
@@ -5,36 +6,38 @@ import { Pressable } from "react-native";
 
 export default function PostLayout() {
     return (
-        <Stack
-            screenOptions={{
-                headerTintColor: colors.BLACK,
-                contentStyle: {
-                    backgroundColor: colors.WHITE,
-                },
-            }}
-        >
-            <Stack.Screen
-                name="write"
-                options={{
-                    title: "글쓰기",
-                    headerShown: true,
-                    headerLeft: () => (
-                        <Pressable
-                            onPress={() => router.replace("/(tabs)/home")}
-                        >
-                            <Feather name="arrow-left" size={24} color={colors.BLACK} />
-                        </Pressable>
-                    ),
+        <AuthRoute>
+            <Stack
+                screenOptions={{
+                    headerTintColor: colors.BLACK,
+                    contentStyle: {
+                        backgroundColor: colors.WHITE,
+                    },
                 }}
-            />
+            >
+                <Stack.Screen
+                    name="write"
+                    options={{
+                        title: "글쓰기",
+                        headerShown: true,
+                        headerLeft: () => (
+                            <Pressable
+                                onPress={() => router.replace("/(tabs)/home")}
+                            >
+                                <Feather name="arrow-left" size={24} color={colors.BLACK} />
+                            </Pressable>
+                        ),
+                    }}
+                />
 
-            <Stack.Screen
-                name="update/[id]"
-                options={{
-                    title: "게시글 수정",
-                    headerShown: true,
-                }}
-            />
-        </Stack>
+                <Stack.Screen
+                    name="update/[id]"
+                    options={{
+                        title: "게시글 수정",
+                        headerShown: true,
+                    }}
+                />
+            </Stack>
+        </AuthRoute>
     );
 }
