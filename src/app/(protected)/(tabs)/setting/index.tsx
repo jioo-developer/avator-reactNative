507696ffadc87668cba1a6432ca5de5594e88290
김지호 <rlawl3383@gmail.com>
2026-04-14 00:33:15 +0900

    setting page ui 추가

--- src/app/(protected)/(tabs)/setting/index.tsx
+++ src/app/(protected)/(tabs)/setting/index.tsx
@@ -1,14 +1,35 @@
+import ListItem from "@/components/_pageComponents/setting/ListItem";
+import AuthRoute from "@/components/AuthRoute";
+import { colors } from "@/constants";
 import useAuth from "@/hooks/queries/auth/useAuth";
-import { Pressable, Text } from "react-native";
+import { Entypo, Octicons } from "@expo/vector-icons";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SettingScreen() {
   const { logout } = useAuth();
+
   return (
-    <SafeAreaView>
-      <Pressable onPress={logout}>
-        <Text>로그아웃</Text>
-      </Pressable>
-    </SafeAreaView>
+    <AuthRoute>
+      <SafeAreaView>
+        <View style={styles.space} />
+        <ListItem
+          title="언어 설정"
+          icon={<Entypo name="language" size={16} color={colors.BLACK} />}
+        />
+        <View style={styles.space} />
+        <ListItem
+          title="로그아웃"
+          onPress={logout}
+          icon={<Octicons name="sign-out" size={16} color={colors.BLACK} />}
+        />
+      </SafeAreaView>
+    </AuthRoute>
   );
 }
+
+const styles = StyleSheet.create({
+  space: {
+    height: 10,
+  },
+});
